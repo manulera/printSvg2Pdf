47291de17442b46600793c5d1a149ab456208c46
Manuel Lera Ramirez <manuel.lera@embl.de>
2021-05-27 15:27:08 +0200

    corrected path of example

--- printSvg2Pdf.sh
+++ printSvg2Pdf.sh
@@ -4,7 +4,7 @@
 chrome_executable="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 
 # Path to the template.html file in this folder
-template_file="/Users/Manu/Documents/Projects/anaphase_yeast/experimental_data/figures_paper/printSvg2Pdf/template.html"
+template_file="/path/to/template.html"
 
 sed "s|__targetfile__|${1}|" "$template_file" > printSvg2Pdf_temp.html
 "$chrome_executable" --headless --disable-gpu --print-to-pdf-no-header --print-to-pdf=$2 printSvg2Pdf_temp.html
